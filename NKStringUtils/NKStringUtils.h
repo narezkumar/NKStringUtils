31d124cea8afed16950eae9359321a440c4aa7af
Naresh Kumar <nareshkumar@hemanth-imac.local>
2016-09-19 11:00:09 +0530

    Initial Commit

---
+++ NKStringUtils/NKStringUtils.h
@@ -0,0 +1,19 @@
+//
+//  NKStringUtils.h
+//  NKStringUtils
+//
+//  Created by Naresh Kumar on 9/19/16.
+//  Copyright © 2016 Naresh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKStringUtils.
+FOUNDATION_EXPORT double NKStringUtilsVersionNumber;
+
+//! Project version string for NKStringUtils.
+FOUNDATION_EXPORT const unsigned char NKStringUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKStringUtils/PublicHeader.h>
+
+
